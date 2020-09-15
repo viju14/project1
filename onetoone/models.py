e952e43cbf15ab9581d15d828bce9879e1e4f0fa
mypython14 <mypython14@gmail.com>
2020-09-15 22:36:07 +0530

    first commit

---
+++ onetoone/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+# Create your models here.
+class Employee(models.Model):
+    name=models.CharField(max_length=20)
+    #address=models.CharField(max_length=50)
+    salary=models.FloatField()
+    age=models.IntegerField()
+
+
+    class Meta:
+        db_table='Employee'
+#
+class Address(models.Model):
+    city=models.CharField(max_length=20)
+    state=models.CharField(max_length=20)
+    cuntry=models.CharField(max_length=20)
+    pincode=models.IntegerField()
+    emp=models.ManyToManyField(Employee,unique=False,related_name='adr',null=True)
+    class Meta:
+        db_table='Address'
+
+        #Address(city='pune'
+
+
+
